70cda8bf4951d18152c23cb46588d3c641c441c4
Your Name <saiguptha_v@srmap.edu.in>
2025-02-18 19:56:13 +0530

    db: added document modal with features like created_at, user_id, content

---
+++ models/document.py
@@ -0,0 +1,12 @@
+from utils import db
+from datetime import datetime, timezone
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Document {self.title}>'
