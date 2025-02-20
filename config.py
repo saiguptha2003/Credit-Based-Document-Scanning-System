d6a6cc40b3cbd8a32e09139a9a7e674330db8d00
Your Name <saiguptha_v@srmap.edu.in>
2025-02-21 00:02:49 +0530

    modify: added the jwt related configuration

--- config.py
+++ config.py
@@ -9,3 +9,29 @@ class Config:
     UPLOAD_FOLDER = 'uploads'
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024 
     DAILY_FREE_CREDITS = 20
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or '219d86a1-28c9-4991-a4e6-b3be133feab1'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+    JWT_BLACKLIST_ENABLED = True
+    JWT_BLACKLIST_TOKEN_CHECKS = ['access', 'refresh']
+    JWT_TOKEN_LOCATION = ['headers']
+    JWT_HEADER_NAME = 'Authorization'
+    JWT_HEADER_TYPE = 'Bearer'
+    JWT_COOKIE_SECURE = True
+    JWT_COOKIE_SAMESITE = 'Lax'
+    JWT_COOKIE_HTTPONLY = True
+    JWT_COOKIE_PATH = '/'
+    JWT_COOKIE_CSRF_PROTECT = True
+    JWT_COOKIE_SECURE = True
+    JWT_COOKIE_SAMESITE = 'Lax'
+    JWT_COOKIE_HTTPONLY = True
+    JWT_COOKIE_PATH = '/'
+    JWT_COOKIE_CSRF_PROTECT = True
+    JWT_COOKIE_SECURE = True
+    JWT_COOKIE_SAMESITE = 'Lax'
+    JWT_COOKIE_HTTPONLY = True
+    JWT_COOKIE_PATH = '/'
+    JWT_COOKIE_CSRF_PROTECT = True
+    
+    
+
